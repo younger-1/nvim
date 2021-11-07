6bd546cd566fb29e22c6f7fc3286832186cb5394
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-11-08 02:58:16 +0800

    [feat] keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,45 +1,227 @@
-vim.g.mapleader = ' '
+local M = {}
+-- local Log = require 'lvim.core.log'
 
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local generic_opts_any = { noremap = true, silent = true }
 
--- General Keymaps
-keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', opts) -- Open file explorer
-keymap('n', '<leader>w', ':w<CR>', opts)
-keymap('n', '<leader>q', ':q<CR>', opts)
-keymap('n', '<leader>c', ':bd<CR>', opts)
-keymap('n', 'H', ':BufferPrevious<CR>', opts) -- Toggle TreeSitter playground
-keymap('n', 'L', ':BufferNext<CR>', opts) -- Toggle TreeSitter playground
+local generic_opts = {
+  insert_mode = generic_opts_any,
+  normal_mode = generic_opts_any,
+  visual_mode = generic_opts_any,
+  visual_block_mode = generic_opts_any,
+  command_mode = generic_opts_any,
+  term_mode = { silent = true },
+  operator_mode = generic_opts_any,
+  map_mode = generic_opts_any,
+  insert_command_mode = generic_opts_any,
+}
 
--- Move selected line / block of text in visual mode
-keymap('x', '<C-Up>', ":move '<-2<CR>gv-gv", opts)
-keymap('x', '<C-Down>', ":move '>+1<CR>gv-gv", opts)
+local mode_adapters = {
+  insert_mode = 'i',
+  normal_mode = 'n',
+  visual_mode = 'v',
+  visual_block_mode = 'x',
+  command_mode = 'c',
+  term_mode = 't',
+  operator_mode = 'o',
+  map_mode = '',
+  insert_command_mode = '!',
+}
 
--- Move line in insert mode
-keymap('i', '<C-Up>', '<C-o>:m .-2<CR>', opts)
-keymap('i', '<C-Down>', '<C-o>:m .+1<CR>', opts)
+-- Append key mappings to lunarvim's defaults for a given mode
+-- @param keymaps The table of key mappings containing a list per mode (normal_mode, insert_mode, ..)
+-- function M.append_to_defaults(keymaps)
+--   for mode, mappings in pairs(keymaps) do
+--     for k, v in pairs(mappings) do
+--       lvim.keys[mode][k] = v
+--     end
+--   end
+-- end
 
--- Telescope keymaps
-keymap('n', ' sf', ':Telescope find_files<CR>', opts) -- Find files
-keymap('n', ' sg', ':Telescope live_grep<CR>', opts)
-keymap('n', ' sr', ':Telescope oldfiles<CR>', opts)
-keymap('n', ' go', ':Telescope git_status<CR>', opts)
+-- Set key mappings individually
+-- @param mode The keymap mode, can be one of the keys of mode_adapters
+-- @param key The key of keymap
+-- @param val Can be form as a mapping or tuple of mapping and user defined opt
+function M.set_keymap(mode, key, val)
+  local opt = generic_opts[mode] or generic_opts_any
+  mode = mode_adapters[mode] or mode
+  if type(val) == 'table' then
+    opt = val[2]
+    val = val[1]
+  end
+  vim.api.nvim_set_keymap(mode, key, val, opt)
+end
 
--- Toggle to disable mouse mode and indentlines for easier paste
-ToggleMouse = function()
-  if vim.o.mouse == 'nvi' then
-    -- vim.cmd[[IndentBlanklineDisable]]
-    vim.wo.signcolumn = 'no'
-    vim.o.mouse = ''
-    vim.wo.number = false
-    print 'Mouse disabled'
-  else
-    -- vim.cmd[[IndentBlanklineEnable]]
-    vim.wo.signcolumn = 'yes'
-    vim.o.mouse = 'nvi'
-    vim.wo.number = true
-    print 'Mouse enabled'
+-- Load key mappings for a given mode
+-- @param mode The keymap mode, can be one of the keys of mode_adapters
+-- @param keymaps The list of key mappings
+local load_mode = function(mode, keymaps)
+  for k, v in pairs(keymaps) do
+    M.set_keymap(mode, k, v)
   end
 end
 
-vim.api.nvim_set_keymap('n', 'yom', '<cmd>lua ToggleMouse()<cr>', { noremap = true })
+-- Load key mappings for all provided modes
+-- @param keymaps A list of key mappings for each mode
+function M.load(keymaps)
+  keymaps = keymaps or {}
+  for mode, mapping in pairs(keymaps) do
+    load_mode(mode, mapping)
+  end
+end
+
+function M.config()
+  local keys = {
+    insert_command_mode = {
+      ['<C-a>'] = '<Home>',
+      ['<C-e>'] = '<End>',
+      --
+      ['<A-f>'] = '<S-Right>',
+      ['<A-b>'] = '<S-Left>',
+      -- ["<C-b>"] = "<Left>",
+      -- ["<C-f>"] = "<Right>",
+    },
+    ---@usage change or add keymappings for command mode
+    command_mode = {
+      -- navigate tab completion with <c-j> and <c-k>
+      -- runs conditionally
+      ['<C-j>'] = { 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true } },
+      ['<C-k>'] = { 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true } },
+      --
+      ['<C-V>'] = '<C-R>+',
+    },
+
+    -- ---@usage change or add keymappings for insert mode
+    insert_mode = {
+      -- 'jk' for quitting insert mode
+      ['jk'] = '<ESC>',
+      -- 'kj' for quitting insert mode
+      ['kj'] = '<ESC>',
+      -- 'jj' for quitting insert mode
+      ['jj'] = '<ESC>',
+      -- Move current line / block with Alt-j/k ala vscode.
+      ['<A-j>'] = '<Esc>:m .+1<CR>==gi',
+      -- Move current line / block with Alt-j/k ala vscode.
+      ['<A-k>'] = '<Esc>:m .-2<CR>==gi',
+      -- navigation
+      ['<A-Up>'] = '<C-\\><C-N><C-w>k',
+      ['<A-Down>'] = '<C-\\><C-N><C-w>j',
+      ['<A-Left>'] = '<C-\\><C-N><C-w>h',
+      ['<A-Right>'] = '<C-\\><C-N><C-w>l',
+      -- break undo sequence, start new change
+      ['<C-U>'] = '<C-G>u<C-U>',
+      ['<C-W>'] = '<C-G>u<C-W>',
+      ['<C-v>'] = '<C-G>u<C-R><C-O>+',
+    },
+
+    ---@usage change or add keymappings for normal mode
+    normal_mode = {
+      -- Better window movement
+      ['<C-h>'] = '<C-w>h',
+      ['<C-j>'] = '<C-w>j',
+      ['<C-k>'] = '<C-w>k',
+      ['<C-l>'] = '<C-w>l',
+
+      -- Resize with arrows
+      ['<C-Up>'] = ':resize -2<CR>',
+      ['<C-Down>'] = ':resize +2<CR>',
+      ['<C-Left>'] = ':vertical resize -2<CR>',
+      ['<C-Right>'] = ':vertical resize +2<CR>',
+
+      -- Move current line / block with Alt-j/k a la vscode.
+      ['<A-j>'] = ':m .+1<CR>==',
+      ['<A-k>'] = ':m .-2<CR>==',
+
+      -- QuickFix
+      [']q'] = ':cnext<CR>',
+      ['[q'] = ':cprev<CR>',
+      ['<C-q>'] = ':call QuickFixToggle()<CR>',
+
+      [']e'] = ':lnext<CR>',
+      ['[e'] = ':lprev<CR>',
+      ['<C-e>'] = ':call LocListToggle()<CR>',
+
+      -- Remap for dealing with word wrap
+      k = { "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true } },
+      j = { "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true } },
+
+      --
+      Y = 'y$',
+      ZA = ':wqa<CR>',
+      ['g<C-l>'] = '<cmd>nohlsearch<cr><C-l>',
+      ['+'] = ':SymbolsOutline<cr>',
+    },
+
+    ---@usage change or add keymappings for terminal mode
+    term_mode = {
+      -- Terminal window navigation
+      ['<C-h>'] = '<C-\\><C-N><C-w>h',
+      ['<C-j>'] = '<C-\\><C-N><C-w>j',
+      ['<C-k>'] = '<C-\\><C-N><C-w>k',
+      ['<C-l>'] = '<C-\\><C-N><C-w>l',
+
+      -- quitting insert mode
+      JJ = '<C-\\><C-N>',
+      JK = '<C-\\><C-N>',
+    },
+
+    ---@usage change or add keymappings for visual mode
+    visual_mode = {
+      -- Better indenting
+      ['<'] = '<gv',
+      ['>'] = '>gv',
+
+      Y = [["+y]],
+      d = [["_d]],
+      X = [["+x]],
+
+      -- Select to start and end quickly
+      H = '^',
+      L = '$',
+    },
+
+    ---@usage change or add keymappings for visual block mode
+    visual_block_mode = {
+      -- Move selected line / block of text in visual mode
+      ['K'] = ":move '<-2<CR>gv-gv",
+      ['J'] = ":move '>+1<CR>gv-gv",
+
+      -- Move current line / block with Alt-j/k ala vscode.
+      ['<A-j>'] = ":m '>+1<CR>gv-gv",
+      ['<A-k>'] = ":m '<-2<CR>gv-gv",
+    },
+
+    operator_mode = {
+      -- Operate to start and end quickly
+      H = '^',
+      L = '$',
+    },
+
+    map_mode = {},
+  }
+
+  if vim.fn.has 'mac' == 1 then
+    keys.normal_mode['<A-Up>'] = keys.normal_mode['<C-Up>']
+    keys.normal_mode['<A-Down>'] = keys.normal_mode['<C-Down>']
+    keys.normal_mode['<A-Left>'] = keys.normal_mode['<C-Left>']
+    keys.normal_mode['<A-Right>'] = keys.normal_mode['<C-Right>']
+    -- Log:debug 'Activated mac keymappings'
+  end
+
+  M.keys = keys
+end
+
+function M.print(mode)
+  print "List of LunarVim's default keymappings (not including which-key)"
+  if mode then
+    print(vim.inspect(M.keys[mode]))
+  else
+    print(vim.inspect(M.keys))
+  end
+end
+
+function M.setup()
+  M.config()
+  M.load(M.keys)
+end
+
+return M
