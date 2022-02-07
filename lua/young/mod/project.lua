ca277bbddba5874aef1484f7a04dffb5c44bf538
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-07 19:43:28 +0800

    [key] project root

--- lua/young/mod/project.lua
+++ lua/young/mod/project.lua
@@ -3,13 +3,13 @@ local project = require 'project_nvim'
 local cfg = {
   -- Manual mode doesn't automatically change your root directory, so you have
   -- the option to manually do so using `:ProjectRoot` command.
-  manual_mode = false,
+  manual_mode = true,
 
   -- Methods of detecting the root directory. **"lsp"** uses the native neovim
   -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
   -- order matters: if one is not detected, the other is used as fallback. You
   -- can also delete or rearangne the detection methods.
-  detection_methods = { 'pattern', 'lsp' },
+  detection_methods = { 'lsp', 'pattern' },
 
   -- All the patterns used to detect root dir, when **"pattern"** is in
   -- detection_methods
@@ -30,7 +30,7 @@ local cfg = {
 
   -- When set to false, you will get a message when project.nvim changes your
   -- directory.
-  silent_chdir = true,
+  silent_chdir = false,
 
   -- Path where project.nvim will store the project history for use in
   -- telescope
