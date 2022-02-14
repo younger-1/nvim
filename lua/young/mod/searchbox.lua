9c85eb3529e988b562e4210a232d4d191703657d
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-14 16:41:01 +0800

    [searchbox] inner mapping

--- lua/young/mod/searchbox.lua
+++ lua/young/mod/searchbox.lua
@@ -3,7 +3,7 @@ local xmap = require('young.key').xmap
 
 nmap('<C-f>', ':SearchBoxMatchAll<CR>')
 xmap('<C-f>', ':SearchBoxMatchAll visual_mode=true<CR>')
-nmap('<C-b>', ':SearchBoxMatchAll exact=true title=" Search 🦝 "<CR>')
+-- nmap('<C-b>', ':SearchBoxMatchAll exact=true title=" Search 🦝 "<CR>')
 
 require('searchbox').setup({
   popup = {
@@ -29,8 +29,18 @@ require('searchbox').setup({
     before_mount = function(input)
       -- code
     end,
+    -- <https://github.com/VonHeikemen/searchbox.nvim/issues/24>
     after_mount = function(input)
-      -- code
+      local map = function(lhs, rhs)
+        vim.api.nvim_buf_set_keymap(input.bufnr, 'i', lhs, rhs, {noremap = false})
+      end
+
+      local mode = "<Esc><cmd>sleep 3m<CR><cmd>SearchBox%s<CR>"
+
+      map('<C-a>', mode:format('MatchAll'))
+      map('<C-r>', mode:format('IncSearch reverse=true'))
+      map('<C-x>', mode:format('IncSearch exact=true'))
+      -- map('<CR>' , mode:format('MatchAll -- <C-r>=expand("<cword>")<CR>'))
     end,
     on_done = function(value, search_type)
       if value == nil then return end
