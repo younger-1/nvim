bfcbaf4be293f5ab23324d40273b8f924822426c
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-17 20:19:03 +0800

    [new] minimap

---
+++ lua/young/mod/minimap.lua
@@ -0,0 +1,7 @@
+-- vim.g.minimap_auto_start = 1
+-- vim.g.minimap_auto_start_win_enter = 1
+vim.g.minimap_width = 12
+vim.g.minimap_close_filetypes = { 'startify', 'netrw', 'vim-plug', 'dashboard', 'alpha' }
+vim.g.minimap_highlight_range = 1 -- highlight range of visible lines
+vim.g.minimap_highlight_search = 1
+vim.g.minimap_git_colors = 1
