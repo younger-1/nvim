0e03e7bf39cecde3644f5f269e35068f6e4dd639
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-20 10:54:39 +0800

    [fix] wilder

--- lua/young/mod/wilder.lua
+++ lua/young/mod/wilder.lua
@@ -1,7 +1,8 @@
 vim.cmd [[
   call wilder#setup({
-  \ 'modes': [':', '/', '?'],
-  \ 'accept_key': '<C-e>',
+  \ 'modes': [':'],
+  \ 'accept_key': '<C-y>',
+  \ 'reject_key': '<C-e>',
   \ })
 
   " 'border'            : 'single', 'double', 'rounded' or 'solid', can also be a list of 8 characters
@@ -22,7 +23,14 @@ vim.cmd [[
   \   'accent': wilder#make_hl('WilderAccent', 'Pmenu', [{}, {}, {'foreground': '#e27878', 'bold': v:true, 'underline': v:true}]),
   \   'selected_accent': wilder#make_hl('WilderSelectedAccent', 'PmenuSel', [{}, {}, {'foreground': '#e27878', 'bold': v:true, 'underline': v:true}]),
   \ },
-  \ 'left': [wilder#popupmenu_devicons({'get_hl': wilder#devicons_get_hl_from_glyph_palette_vim()}), wilder#popupmenu_buffer_flags({'flags': ' '})],
+  \ 'left': [
+  \   ' ',
+  \   wilder#popupmenu_devicons(),
+  \ ],
+  \ 'right': [
+  \   ' ',
+  \   wilder#popupmenu_scrollbar(),
+  \ ],
   \ }))
 
   let s:wilder_search_renderer = wilder#wildmenu_renderer({
@@ -32,8 +40,7 @@ vim.cmd [[
   \ 'right': [' ', wilder#wildmenu_index()],
   \ })
 
-  call wilder#set_option(
-  \ 'renderer',
+  call wilder#set_option('renderer',
   \ wilder#renderer_mux({
   \   ':': s:wilder_cmd_line_renderer,
   \   '/': s:wilder_search_renderer,
@@ -41,24 +48,24 @@ vim.cmd [[
   \ })
   \ )
 
-  call wilder#set_option('pipeline', [
-  \ wilder#branch(
-  \   wilder#python_file_finder_pipeline({
-  \     'file_command': {_, arg -> stridx(arg, '.') != -1 ? ['fd', '-tf', '-H', '--strip-cwd-prefix'] : ['fd', '-tf', '--strip-cwd-prefix']},
-  \     'dir_command': ['fd', '-td'],
-  \   }),
-  \   wilder#cmdline_pipeline({'fuzzy': 1}),
-  \   [
-  \     wilder#check({_, x -> empty(x)}),
-  \     wilder#history(),
-  \   ],
-  \   wilder#python_search_pipeline({
-  \     'pattern': wilder#python_fuzzy_pattern({
-  \       'start_at_boundary': 0,
-  \     }),
-  \   }),
-  \   wilder#vim_search_pipeline()
-  \ ),
-  \ ])
+  " call wilder#set_option('pipeline', [
+  " \ wilder#branch(
+  " \   wilder#python_file_finder_pipeline({
+  " \     'file_command': {_, arg -> stridx(arg, '.') != -1 ? ['fd', '-tf', '-H'] : ['fd', '-tf']},
+  " \     'dir_command': ['fd', '-td'],
+  " \   }),
+  " \   wilder#cmdline_pipeline({'fuzzy': 1}),
+  " \   [
+  " \     wilder#check({_, x -> empty(x)}),
+  " \     wilder#history(),
+  " \   ],
+  " \   wilder#python_search_pipeline({
+  " \     'pattern': wilder#python_fuzzy_pattern({
+  " \       'start_at_boundary': 0,
+  " \     }),
+  " \   }),
+  " \   wilder#vim_search_pipeline()
+  " \ ),
+  " \ ])
 
 ]]
