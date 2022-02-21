4e2d1e7f9034a83fe770ef003d601a45f672235e
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-21 19:34:35 +0800

    [lsp] pylsp settings

--- lua/young/lsp/python.lua
+++ lua/young/lsp/python.lua
@@ -15,6 +15,16 @@ M.opts = {
         completeFunctionParens = true,
       },
     },
+    -- pylsp = {
+    --   plugins = {
+    --     pylint = { enabled = true, executable = "pylint" },
+    --     pyflakes = { enabled = false },
+    --     pycodestyle = { enabled = false },
+    --     jedi_completion = { fuzzy = true },
+    --     pyls_isort = { enabled = true },
+    --     pylsp_mypy = { enabled = true },
+    --   },
+    -- },
   },
   -- before_init = function(params, config)
   --   M.env(config.root_dir)
