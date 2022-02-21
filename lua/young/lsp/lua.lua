3e31b4580ec5679dea32a5d0629e8433466e8f0a
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-21 19:45:35 +0800

    [lsp] lua path

--- lua/young/lsp/lua.lua
+++ lua/young/lsp/lua.lua
@@ -12,14 +12,17 @@ M.opts = {
       },
       workspace = {
         library = {
-          [vim.fn.expand "$VIMRUNTIME"] = true,
-          [_G.packer_plugins["lua-dev.nvim"].path] = true,
-          [_G.packer_plugins["plenary.nvim"].path] = true,
-          -- [_G.packer_plugins["telescope.nvim"].path] = true,
+          vim.fn.expand "$VIMRUNTIME",
+          _G.packer_plugins["lua-dev.nvim"].path,
+          _G.packer_plugins["plenary.nvim"].path,
+          -- _G.packer_plugins["telescope.nvim"].path,
         },
         maxPreload = 20000,
         preloadFileSize = 2000,
       },
+      -- workspace = {
+      --   library = vim.api.nvim_get_runtime_file("", true),
+      -- },
       diagnostics = {
         globals = {
           'vim',
