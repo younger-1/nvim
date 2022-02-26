689f35e4f2d9cceefe66a0e6c9c688c5d7670a5b
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-02-26 14:15:15 +0800

    [telescope] settings

---
+++ lua/young/mod/telescope/actions.lua
@@ -0,0 +1,11 @@
+-- TODO: https://github.com/nvim-telescope/telescope.nvim/pull/1084#issuecomment-913849414
+local action_mt = require "telescope.actions.mt"
+local action_state = require "telescope.actions.state"
+
+local M = action_mt.transform_mod {
+  print_entry = function()
+    print(vim.inspect(action_state.get_selected_entry()))
+  end,
+}
+
+return M
