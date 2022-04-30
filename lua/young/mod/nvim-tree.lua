9c85e83b23331cd06178d92c8478abc95d30b19e
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-04-30 23:21:04 +0800

    [nvim-tree] update

--- lua/young/mod/nvim-tree.lua
+++ lua/young/mod/nvim-tree.lua
@@ -20,36 +20,38 @@ M.opts = {
       ignored = '',
     },
   },
-  respect_buf_cwd = 1,
-  create_in_closed_folder = 0,
-  -- root_folder_modifier = ':t',
-  -- TODO: not impl
-  -- ignore = { '.git', 'node_modules', '.cache' },
-  -- hide_dotfiles = 1,
+  special_files = {
+    ["Cargo.toml"] = true,
+    Makefile = true,
+    ["README.md"] = true,
+    ["readme.md"] = true,
+  }
 }
 
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 M.cfg = {
+  update_cwd = true, -- Changes the tree root directory on DirChanged
+  update_focused_file = {
+    enable = true,
+    ignore_list = {},
+  },
   disable_netrw = false,
   hijack_netrw = false,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    'alpha',
-    'dashboard',
-  },
-  auto_close = false,
-  update_cwd = true,
   -- hijacks new directory buffers when they are opened
   -- disable it if using `vim-dirvish` or `lir.nvim`
   hijack_directories = {
     enable = true,
     auto_open = true,
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = false,
-    ignore_list = {},
+  -- open_on_setup = true,
+  -- open_on_setup_file = true,
+  -- ignore_ft_on_setup = {
+  --   'alpha',
+  --   'dashboard',
+  -- },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
   },
   diagnostics = {
     enable = true,
@@ -60,20 +62,9 @@ M.cfg = {
       error = '',
     },
   },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
   actions = {
     change_dir = {
+      enable = false,
       global = false,
     },
     open_file = {
@@ -86,19 +77,29 @@ M.cfg = {
       },
     },
   },
+  git = {
+    enable = true,
+    ignore = true, -- Toggle via the `toggle_git_ignored` action, default mapping I
+  },
+  filters = {
+    dotfiles = false, -- Toggle via the `toggle_dotfiles` action, default mapping H
+    custom = {  -- Toggle via the `toggle_custom` action, default mapping U
+      '^\\.git'
+    },
+    exclude = {}
+  },
   view = {
     side = 'left',
     hide_root_folder = false,
-    auto_resize = false, -- false good
     mappings = {
       custom_only = false,
       list = {
         { key = 'l', action = 'edit' },
         { key = 'h', action = 'close_node' },
         { key = 'u', action = 'parent_node' },
-        { key = 'i', action = 'cd' },
         { key = '?', action = 'toggle_help' },
-        { key = '.', action = 'toggle_dotfiles' },
+        { key = 'i', action = 'toggle_file_info' },
+        { key = 'C', action = 'toggle_custom' },
         { key = 'P', action = 'print_path', action_cb = print_node_path },
       },
     },
