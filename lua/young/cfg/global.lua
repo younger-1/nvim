3fe92cbe90372a91c82442583ab01068c061f906
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-05-06 15:37:39 +0800

    [key] gx

--- lua/young/cfg/global.lua
+++ lua/young/cfg/global.lua
@@ -22,8 +22,8 @@ local disable_distribution = function()
   vim.g.loaded_matchparen = 1
 
   vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwFileHandlers = 1
   vim.g.loaded_netrwPlugin = 1
+  vim.g.loaded_netrwFileHandlers = 1
   vim.g.loaded_netrwSettings = 1
 
   vim.g.loaded_spellfile_plugin = 1
