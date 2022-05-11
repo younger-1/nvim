16dcd3f108180cb3f6103c14841575efcf637e98
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-05-11 16:49:00 +0800

    [core] young autocmd and map

--- lua/young/utils/global.lua
+++ lua/young/utils/global.lua
@@ -3,6 +3,102 @@ _G.young = {}
 _G.uv = vim.loop
 _G.api = vim.api
 
+----------------------------------------------------------------------------------------------------
+-- Autocommand
+----------------------------------------------------------------------------------------------------
+---@class Autocommand
+---@field description string
+---@field event   string[] list of autocommand events
+---@field pattern string[] list of autocommand patterns
+---@field command string | function
+---@field nested  boolean
+---@field once    boolean
+---@field buffer  number
+---@field desc    number
+
+---Create an autocommand
+---returns the group ID so that it can be cleared or manipulated.
+---@param name string
+---@param commands Autocommand[]
+---@return number
+function young.augroup(name, commands)
+  local id = api.nvim_create_augroup(name, { clear = true })
+  for _, autocmd in ipairs(commands) do
+    local is_callback = type(autocmd.command) == 'function'
+    api.nvim_create_autocmd(autocmd.event, {
+      group = name,
+      pattern = autocmd.pattern,
+      desc = autocmd.desc,
+      callback = is_callback and autocmd.command or nil,
+      command = not is_callback and autocmd.command or nil,
+      once = autocmd.once,
+      nested = autocmd.nested,
+      buffer = autocmd.buffer,
+    })
+  end
+  return id
+end
+
+----------------------------------------------------------------------------------------------------
+-- Mappings
+----------------------------------------------------------------------------------------------------
+
+-- source: https://github.com/akinsho/dotfiles/blob/main/.config/nvim/lua/as/globals.lua
+-- credits: akinsho
+---create a mapping function factory
+---@param mode string
+---@param o table
+---@return fun(lhs: string, rhs: string|function, opts: table|nil) 'create a mapping'
+local function make_mapper(mode, o)
+  -- copy the opts table as extends will mutate the opts table passed in otherwise
+  local parent_opts = vim.deepcopy(o)
+  ---Create a mapping
+  ---@param lhs string
+  ---@param rhs string|function
+  ---@param opts table
+  return function(lhs, rhs, opts)
+    -- If the label is all that was passed in, set the opts automagically
+    opts = type(opts) == 'string' and { desc = opts } or opts and vim.deepcopy(opts) or {}
+    vim.keymap.set(mode, lhs, rhs, vim.tbl_extend('keep', opts, parent_opts))
+  end
+end
+
+local map_opts = { remap = true, silent = true }
+local noremap_opts = { silent = true }
+
+-- A recursive normal mapping
+young.nmap = make_mapper('n', map_opts)
+-- A recursive select mapping
+young.xmap = make_mapper('x', map_opts)
+-- A recursive insert mapping
+young.imap = make_mapper('i', map_opts)
+-- A recursive visual mapping
+young.vmap = make_mapper('v', map_opts)
+-- A recursive operator mapping
+young.omap = make_mapper('o', map_opts)
+-- A recursive terminal mapping
+young.tmap = make_mapper('t', map_opts)
+-- A recursive visual & select mapping
+young.smap = make_mapper('s', map_opts)
+-- A recursive commandline mapping
+young.cmap = make_mapper('c', { remap = true, silent = false })
+-- A non recursive normal mapping
+young.nnoremap = make_mapper('n', noremap_opts)
+-- A non recursive select mapping
+young.xnoremap = make_mapper('x', noremap_opts)
+-- A non recursive visual mapping
+young.vnoremap = make_mapper('v', noremap_opts)
+-- A non recursive insert mapping
+young.inoremap = make_mapper('i', noremap_opts)
+-- A non recursive operator mapping
+young.onoremap = make_mapper('o', noremap_opts)
+-- A non recursive terminal mapping
+young.tnoremap = make_mapper('t', noremap_opts)
+-- A non recursive visual & select mapping
+young.snoremap = make_mapper('s', noremap_opts)
+-- A non recursive commandline mapping
+young.cnoremap = make_mapper('c', { silent = false })
+
 --[[
 Windows:
   machine = "x86_64",
