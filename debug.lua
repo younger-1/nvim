10bab43c922d6e9f9b69e34247d196b040f7af2c
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-08-02 13:29:43 +0800

    [clean]: util.global -> young

--- debug.lua
+++ debug.lua
@@ -19,3 +19,47 @@ end)
 
 -- Should not be required, not in lua runtime.path
 require 'plenary.filetypes.base'
+
+local x = {
+  a = 3,
+  b = 4,
+}
+
+local mt = {}
+-- mt.__index = {
+--   c = 5,
+--   a = 6,
+-- }
+function mt:__index(k)
+  pp(self)
+  pp(k)
+end
+
+setmetatable(x, mt)
+
+pp(x.a)
+pp(x.b)
+pp(x.c)
+
+local mt = {}
+-- function mt.__index(t, k)
+function mt:__index(k)
+  if self[k] == nil then
+    local ok, ret = pcall(require, 'young.' .. (self._path or '') .. k)
+    -- No use: xy = ok and ret or { no = true }
+    if ok then
+      self[k] = ret
+    else
+      self[k] = { _nil = true, _path = (self._path or '') .. k .. '.' }
+      setmetatable(self[k], mt)
+    end
+  end
+  return self[k]
+end
+
+setmetatable(xy, mt)
+
+--[[
+xy.a -> require('xy').a -> require('xy.a')
+xy.a.b -> require('xy').a.b -> require('xy.a').b -> require('xy.a.b')
+]]
