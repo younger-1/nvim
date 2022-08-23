839e5906746a80d76b92062ebc6818780e1431de
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-08-24 00:44:27 +0800

    [fix] lsp_lines

--- lua/young/mod/lsp_lines.lua
+++ lua/young/mod/lsp_lines.lua
@@ -1,25 +1,24 @@
 local M = {}
 
-local init = true
+require('lsp_lines').setup()
+
 local state = 2
 
 M.toggle = function(cur)
-  if init then
-    require('lsp_lines').register_lsp_virtual_lines()
-    init = false
-  end
-
   state = cur or state
+
   if state == 0 then
     vim.diagnostic.config { virtual_text = false, virtual_lines = false }
-    state = 1
   elseif state == 1 then
     vim.diagnostic.config { virtual_text = true, virtual_lines = false }
-    state = 2
   elseif state == 2 then
     vim.diagnostic.config { virtual_text = false, virtual_lines = true }
-    state = 0
   end
+
+  state = state + 1
+  state = state % 3
 end
+
+M.toggle()
 
 return M
