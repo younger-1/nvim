0f3507797f4d8165ea8642caed81d24c31e9cd38
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-09-07 20:39:30 +0800

    [fix] NvChad/nvim-colorizer

--- lua/young/mod/lightspeed.lua
+++ lua/young/mod/lightspeed.lua
@@ -1,5 +1,5 @@
 require('lightspeed').setup {
-  -- ignore_case = true,
+  ignore_case = true,
   jump_to_unique_chars = { safety_timeout = 400 },
   limit_ft_matches = 6,
   -- repeat_ft_with_target_char = true,
