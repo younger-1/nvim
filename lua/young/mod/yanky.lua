e2dd7a0cd62d310143091f39c172047569b65a7a
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-09-11 14:20:32 +0800

    [hydra] yanky done

--- lua/young/mod/yanky.lua
+++ lua/young/mod/yanky.lua
@@ -1,34 +1,36 @@
 require('yanky').setup {
-  ring = {
-    history_length = 100,
-    storage = 'shada',
-    sync_with_numbered_registers = true,
-    cancel_event = 'update',
-  },
-  system_clipboard = {
-    sync_with_ring = true,
-  },
-  picker = {
-    select = {
-      action = nil, -- nil to use default put action
-    },
-    telescope = {
-      mappings = nil, -- nil to use default mappings
-    },
-  },
-  highlight = {
-    on_put = true,
-    on_yank = true,
-    timer = 500,
-  },
-  preserve_cursor_position = {
-    enabled = true,
-  },
+  -- ring = {
+  --   history_length = 100,
+  --   storage = 'shada',
+  --   sync_with_numbered_registers = true,
+  --   cancel_event = 'update',
+  -- },
+  -- system_clipboard = {
+  --   sync_with_ring = true,
+  -- },
+  -- picker = {
+  --   select = {
+  --     action = nil, -- nil to use default put action
+  --   },
+  --   telescope = {
+  --     mappings = nil, -- nil to use default mappings
+  --   },
+  -- },
+  -- highlight = {
+  --   on_put = true,
+  --   on_yank = true,
+  --   timer = 500,
+  -- },
+  -- preserve_cursor_position = {
+  --   enabled = true,
+  -- },
 }
+
+xy.map.n { 'p', require('young.mod.hydra.yanky').put_after, '[Hydra] Yanky paste' }
+xy.map.n { 'P', require('young.mod.hydra.yanky').put_before, '[Hydra] Yanky paste' }
 
 -- require('telescope').load_extension 'yank_history'
 
-xy.map.n { 'p', require 'young.mod.hydra.yanky', '[Hydra] Yanky paste' }
 -- xy.map { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' } }
 -- xy.map { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' } }
 -- xy.map { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' } }
