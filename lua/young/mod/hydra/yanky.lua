3b19e5499c74d101d08b65b6e966a0e2b592f4ae
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-09-11 14:20:32 +0800

    [hydra] yanky done

--- lua/young/mod/hydra/yanky.lua
+++ lua/young/mod/hydra/yanky.lua
@@ -3,6 +3,10 @@ local Hydra = require 'hydra'
 local yanky_hydra = Hydra {
   name = 'Yank ring',
   mode = 'n',
+  -- config = {
+  --   invoke_on_body = true,
+  -- },
+  -- body = 'p',
   heads = {
     { 'p', '<Plug>(YankyPutAfter)', { desc = 'Paste before' } },
     { 'P', '<Plug>(YankyPutBefore)', { desc = 'Paste after' } },
@@ -11,8 +15,16 @@ local yanky_hydra = Hydra {
   },
 }
 
-local function do_yanky()
+local M = {}
+
+M.put_after = function()
+  vim.fn.feedkeys(xy.util.t '<Plug>(YankyPutAfter)')
   yanky_hydra:activate()
 end
 
-return do_yanky
+M.put_before = function()
+  vim.fn.feedkeys(xy.util.t '<Plug>(YankyPutBefore)')
+  yanky_hydra:activate()
+end
+
+return M
