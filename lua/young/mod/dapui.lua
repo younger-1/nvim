dc9b79df45fb21557b4f241a883c0ec8689ec4e3
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-09-16 01:03:02 +0800

    [clean]

--- lua/young/mod/dapui.lua
+++ lua/young/mod/dapui.lua
@@ -53,3 +53,13 @@ require('dapui').setup {
     max_type_length = nil, -- Can be integer or nil.
   },
 }
+
+-- dap.listeners.after.event_initialized['dapui_config'] = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated['dapui_config'] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited['dapui_config'] = function()
+--   dapui.close()
+-- end
