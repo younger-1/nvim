3a52c92acac0ef8c39c29e944cdad5c2525ad121
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-10-03 22:38:36 +0800

    [todo] edit git-file for windows

--- lua/young/mod/unception.lua
+++ lua/young/mod/unception.lua
@@ -6,5 +6,14 @@ local args = vim.fn.argv()
 if #args > 0 and args[1]:match '%.git[/\\]' then
   vim.g.unception_disable = 1
 end
+
+-- TODO:
+-- :h clientserver <https://github.com/neovim/neovim/pull/17439/files>
+-- <https://learn.microsoft.com/en-us/windows/win32/ipc/pipe-names>
+-- <https://versprite.com/blog/security-research/microsoft-windows-pipes-intro/>
+if is_windows then
+  vim.g.unception_disable = 1
+end
+
 -- vim.g.unception_delete_replaced_buffer = 1
 vim.g.unception_open_buffer_in_new_tab = 1
