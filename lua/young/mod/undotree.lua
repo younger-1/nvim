9723a644cb9f25ece30a985b069255a592e664b6
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-10-07 09:51:24 +0800

    [undotree] setup

---
+++ lua/young/mod/undotree.lua
@@ -0,0 +1,15 @@
+local g = vim.g
+
+g.undotree_WindowLayout = 4
+-- g.undotree_CustomUndotreeCmd = 'topleft vertical 30 new'
+-- g.undotree_CustomDiffpanelCmd = 'belowright 10 new'
+-- g.undotree_SplitWidth = 30
+-- g.undotree_DiffpanelHeight = 10
+
+g.undotree_HelpLine = 0
+g.undotree_ShortIndicators = 1
+g.undotree_TreeNodeShape = '●'
+g.undotree_TreeVertShape = '│'
+g.undotree_TreeSplitShape = '╱'
+g.undotree_TreeReturnShape = '╲'
+g.undotree_SetFocusWhenToggle = 1
