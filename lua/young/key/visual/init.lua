0a6d6bdea10c43bb5ae314e4cf4a880f79241fed
yangjun <yangjun.321@bytedance.com>
2022-10-23 01:48:01 +0800

    [key] visual: get_visual_selection_by_reg

--- lua/young/key/visual/init.lua
+++ lua/young/key/visual/init.lua
@@ -1,6 +1,14 @@
 local visual = {
   ['\\'] = {
     s = { ':so<cr>', ':source' },
+    -- I = { 'y:lua pp(<C-r>")', 'Inspect' },
+    I = {
+      function()
+        local text = xy.util.get_visual_selection_by_reg()
+        fn.feedkeys(':lua=' .. text)
+      end,
+      'Inspect',
+    },
   },
   g = {
     -- ['/'] = { [["ky/\V<C-r>=escape(@k,'/\')<CR><CR>]], 'Search selection' },
--- lua/young/key/visual/leader.lua
+++ lua/young/key/visual/leader.lua
@@ -1,7 +1,7 @@
 local cmd = require('young.key').cmd
 
 return {
-  -- ['/'] = { '<ESC><CMD>lua ___comment_gc(vim.fn.visualmode())<cr>', 'Comment' },
+  -- ['/'] = { '<ESC><CMD>lua ___comment_gc(fn.visualmode())<cr>', 'Comment' },
   g = {
     name = '+git',
     s = { ':Gitsigns stage_hunk<CR>', 'Stage Hunk' },
@@ -13,34 +13,50 @@ return {
     a = { vim.lsp.buf.range_code_action, 'Code action' },
     f = { vim.lsp.buf.range_formatting, 'Format' },
   },
-  p = {
-    name = '+packer',
-    I = { 'y:lua pp(<C-r>")', 'Inspect' },
-  },
   s = {
     name = '+search',
     f = {
       function()
-        local text = xy.util.get_visual_selection()
+        local text = xy.util.get_visual_selection_by_reg()
         require('telescope.builtin').find_files {}
-        vim.fn.feedkeys(text)
+        fn.feedkeys(text)
+      end,
+      'Find files',
+    },
+    F = {
+      function()
+        local text = xy.util.get_visual_selection_by_reg()
+        require('telescope.builtin').find_files { no_ignore = true }
+        fn.feedkeys(text)
       end,
       'Find files',
     },
     g = {
       function()
-        local text = xy.util.get_visual_selection()
+        local text = xy.util.get_visual_selection_by_reg()
         require('telescope.builtin').live_grep {}
-        vim.fn.feedkeys(text)
+        fn.feedkeys(text)
       end,
       'Grep',
     },
     -- g = { 'y:Telescope live_grep<cr><C-r>"', 'Grep' },
     -- g = { '<Esc><cmd>Telescope live_grep<cr><C-r>=AsciiVirtualSelection()<cr>', 'Grep' },
+    G = {
+      function()
+        local text = xy.util.get_visual_selection_by_reg()
+        require('telescope.builtin').live_grep {
+          additional_args = function()
+            return { '-F' } --fixed-string
+          end,
+        }
+        fn.feedkeys(text)
+      end,
+      'Grep fixed string',
+    },
     t = {
       function()
         require('telescope.builtin').grep_string {
-          search = xy.util.get_visual_selection(),
+          search = xy.util.get_visual_selection_by_reg(),
         }
       end,
       'Search fixed string',
