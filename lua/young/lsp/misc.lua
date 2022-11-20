0b99c9a4964ffc54826be32c7ce5bb6f6b179a1b
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-11-21 01:01:01 +0800

    [new]: exrc.nvim

--- lua/young/lsp/misc.lua
+++ lua/young/lsp/misc.lua
@@ -49,4 +49,45 @@ function M.code_action_better(ignore_null_ls, context)
   vim.lsp.buf.code_action(context)
 end
 
+-- @see <https://muniftanjim.dev/blog/neovim-project-local-config-with-exrc-nvim/>
+---@param server_name string
+---@param settings_patcher fun(settings: table): table
+function M.patch_lsp_settings(server_name, settings_patcher)
+  local function patch_settings(client)
+    client.config.settings = settings_patcher(client.config.settings)
+    client.notify('workspace/didChangeConfiguration', {
+      settings = client.config.settings,
+    })
+  end
+
+  local clients = vim.lsp.get_active_clients { name = server_name }
+  if #clients > 0 then
+    patch_settings(clients[1])
+    return
+  end
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client.name == server_name then
+        patch_settings(client)
+        return true
+      end
+    end,
+  })
+end
+
+--[[ ~/.config/hammerspoon/.nvimrc.lua ]]
+-- require('young.lsp.misc').patch_lsp_settings('sumneko_lua', function(settings)
+--   settings.Lua.diagnostics.globals = { 'hs', 'spoon' }
+--   settings.Lua.workspace.library = {}
+
+--   local hammerspoon_emmpylua_annotations = vim.fn.expand '~/.config/hammerspoon/Spoons/EmmyLua.spoon/annotations'
+--   if vim.fn.isdirectory(hammerspoon_emmpylua_annotations) == 1 then
+--     table.insert(settings.Lua.workspace.library, hammerspoon_emmpylua_annotations)
+--   end
+
+--   return settings
+-- end)
+
 return M
