9eacefb6e9b2dd7b758ee688fafb028a3187a57c
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-12-18 01:24:21 +0800

    [clean] winbar

--- lua/young/cfg/init.lua
+++ lua/young/cfg/init.lua
@@ -20,7 +20,6 @@ function M.done()
   tt()
   require 'young.cfg.global'
   require 'young.cfg.option'
-  require 'young.cfg.winbar'
 
   tt()
   require('young.autocmd').done()
--- lua/young/mod/lir.lua
+++ lua/young/mod/lir.lua
@@ -92,3 +92,26 @@ vim.cmd [[augroup lir-settings]]
 vim.cmd [[  autocmd!]]
 vim.cmd [[  autocmd Filetype lir :lua LirSettings()]]
 vim.cmd [[augroup END]]
+
+local set_winbar = function()
+  local bufnum = vim.fn.winbufnr(vim.fn.winnr())
+  local filetype = vim.fn.getbufvar(bufnum, '&filetype')
+
+  if filetype ~= 'lir' then
+    vim.opt_local.winbar = nil
+  else
+    local folder_name =
+      vim.fn.expand('%:p'):gsub(vim.pesc(vim.loop.cwd() .. '/'), ''):gsub(vim.pesc(vim.fn.expand '$HOME'), '~')
+
+    if #folder_name > 0 then
+      vim.opt_local.winbar = folder_name
+    else
+      vim.opt_local.winbar = vim.fn.expand('%:p'):gsub(vim.pesc(vim.fn.expand '$HOME'), '~')
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinEnter' }, {
+  group = vim.api.nvim_create_augroup('_winbar', {}),
+  callback = set_winbar,
+})
