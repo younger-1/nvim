eb8ed05df3ef7e3142994800f9ba314cde02b24b
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-12-23 09:33:24 +0800

    [new] split join

---
+++ lua/young/mod/treesj.lua
@@ -0,0 +1,39 @@
+require('treesj').setup {
+  -- Use default keymaps
+  -- (<space>m - toggle, <space>j - join, <space>s - split)
+  use_default_keymaps = false,
+
+  -- Node with syntax error will not be formatted
+  check_syntax_error = true,
+
+  -- If line after join will be longer than max value,
+  -- node will not be formatted
+  max_join_length = 200,
+
+  -- hold|start|end:
+  -- hold - cursor follows the node/place on which it was called
+  -- start - cursor jumps to the first symbol of the node being formatted
+  -- end - cursor jumps to the last symbol of the node being formatted
+  cursor_behavior = 'hold',
+
+  -- Notify about possible problems or not
+  notify = true,
+
+  -- langs = langs,
+}
+
+local langs = require('treesj.langs')['presets']
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = '*',
+  callback = function()
+    local opts = { buffer = true }
+    if langs[vim.bo.filetype] then
+      vim.keymap.set('n', 'J', '<cmd>TSJToggle<cr>', opts)
+
+      -- vim.keymap.set('n', 'gss', '<Cmd>TSJSplit<CR>', opts)
+      -- vim.keymap.set('n', 'gsj', '<Cmd>TSJJoin<CR>', opts)
+      -- vim.keymap.set('n', 'gss', '<Cmd>SplitjoinSplit<CR>', opts)
+      -- vim.keymap.set('n', 'gsj', '<Cmd>SplitjoinJoin<CR>', opts)
+    end
+  end,
+})
