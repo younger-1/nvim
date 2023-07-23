534e72cd80fcc3bb2385f3738c6244c3395e2706
yangjun <yangjun.321@bytedance.com>
2023-07-23 21:50:41 +0800

    [lsp] clean

--- lua/young/lsp/mason.lua
+++ lua/young/lsp/mason.lua
@@ -67,8 +67,9 @@ require('mason').setup {
 }
 
 require('mason-lspconfig').setup {
-  -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
+  -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
   -- This setting has no relation with the `automatic_installation` setting.
+  ---@type string[]
   ensure_installed = {},
 
   -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
@@ -78,5 +79,10 @@ require('mason-lspconfig').setup {
   --   - true: All servers set up via lspconfig are automatically installed.
   --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
   --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+  ---@type boolean
   automatic_installation = false,
+
+  -- See `:h mason-lspconfig.setup_handlers()`
+  ---@type table<string, fun(server_name: string)>?
+  handlers = nil,
 }
