5ce3e63aa2a7b75c4bf3b1d61cb4e370ee70d588
yangjun <yangjun.321@bytedance.com>
2023-08-05 11:46:25 +0800

    [enable] smooth scroll

--- lua/young/mod/cinnamon.lua
+++ lua/young/mod/cinnamon.lua
@@ -1,13 +1,17 @@
 require('cinnamon').setup {
   -- KEYMAPS:
   default_keymaps = true, -- Create default keymaps.
-  extra_keymaps = false, -- Create extra keymaps.
+  extra_keymaps = true, -- Create extra keymaps.
   extended_keymaps = false, -- Create extended keymaps.
+  override_keymaps = false, -- The plugin keymaps will override any existing keymaps.
 
   -- OPTIONS:
   always_scroll = false, -- Scroll the cursor even when the window hasn't scrolled.
   centered = true, -- Keep cursor centered in window when using window scrolling.
-  default_delay = 5, -- The default delay (in ms) between each line when scrolling.
+  disabled = false, -- Disables the plugin.
+  default_delay = 7, -- The default delay (in ms) between each line when scrolling.
+  hide_cursor = false, -- Hide the cursor while scrolling. Requires enabling termguicolors!
   horizontal_scroll = true, -- Enable smooth horizontal scrolling when view shifts left or right.
-  scroll_limit = 150, -- Max number of lines moved before scrolling is skipped.
+  max_length = -1, -- Maximum length (in ms) of a command. The line delay will be re-calculated. Setting to -1 will disable this option.
+  scroll_limit = 150, -- Max number of lines moved before scrolling is skipped. Setting to -1 will disable this option.
 }
