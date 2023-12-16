08b6f1eddb04e52206d87f45ccbf1cfeaa3e3908
yangjun <yangjun.321@bytedance.com>
2023-12-16 17:45:02 +0800

    [fold] with ufo

--- lua/young/mod/ufo.lua
+++ lua/young/mod/ufo.lua
@@ -85,10 +85,8 @@ local function customizeSelector(bufnr)
 end
 
 M.done = function()
-  -- vim.o.foldcolumn = '1'
   vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
   vim.o.foldlevelstart = 99
-  vim.o.foldenable = true
   vim.opt.fillchars:append [[fold: ,foldopen:,foldsep: ,foldclose:]]
 
   require('ufo').setup {
