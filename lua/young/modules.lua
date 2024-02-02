fc2a69fa4758c78e868aa300d425cd6d80f35c92
yangjun <yangjun.321@bytedance.com>
2024-02-02 13:40:46 +0800

    [new] symbol-usage

--- lua/young/modules.lua
+++ lua/young/modules.lua
@@ -1832,6 +1832,11 @@ modules.LSP = {
     --     }
     --   end,
     -- },
+    {
+      'Wansmer/symbol-usage.nvim',
+      event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+      auto = 'config',
+    },
   },
   highlight = {
     { -- Highlighting the word under the cursor
